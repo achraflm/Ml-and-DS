8227c0e45711f76e1f3b8c4a4e007f47c0ae39f2
Achraf_lm <87104971+achraflm@users.noreply.github.com>
2026-02-18 22:58:39 +0100

    Update Box-Cox.R

--- Time_series/Box-Cox.R
+++ Time_series/Box-Cox.R
@@ -1,1 +1,11 @@
 
+library(faraway)
+?butterfat
+data(butterfat)
+fullmodel = lm(Butterfat ~ Breed * Age, data = butterfat)
+plot(fullmodel)
+library(MASS)
+bc = boxcox(fullmodel , lambda = seq(-3,3))
+best.lam = bc$x[which(bc$y==max(bc$y))]
+fullmodel.inv= lm((Butterfat)^-1  ~ Breed * Age, data = butterfat)
+plot(fullmodel.inv)
